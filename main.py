d7a45b9645e2d6e3743749ae8004770f9ec29b86
Farhan1706 <vnalvaro24@gmail.com>
2024-01-08 00:04:15 +0700

    Pre-final

--- main.py
+++ main.py
@@ -1,42 +1,52 @@
 import cv2
 from sklearn.neighbors import KNeighborsClassifier
 import pickle
-import numpy as np
 import os
 import csv
 from datetime import datetime
 
 # Inisialisasi KNN dan load model
-facecascade = cv2.CascadeClassifier(cv2.data.haarcascades + './haarcascade_frontalface_alt2.xml')
+facecascade_frontal = cv2.CascadeClassifier(cv2.data.haarcascades + './haarcascade_frontalface_default.xml')
+facecascade_profile = cv2.CascadeClassifier(cv2.data.haarcascades + './haarcascade_profileface.xml')
 
 # Mendapatkan daftar file di dalam folder /data
 folder_path = 'data/'
 user_files = os.listdir(folder_path)
 
-faces = []
-names = []
+faces_and_names = {}  # Dictionary untuk menyimpan data wajah dan nama
 
 # Membaca data wajah dan nama dari setiap file yang sesuai pola
 for file_name in user_files:
-    if file_name.startswith('muka_') and file_name.endswith('.pkl'):
-        user_name = file_name.split('_')[1].split('.')[0]
+    if file_name.endswith('.pkl'):
+        user_name = file_name.split('.')[0]
 
-        # Membaca data wajah
-        muka_file_path = os.path.join(folder_path, file_name)
-        with open(muka_file_path, 'rb') as fileMuka:
-            user_faces = pickle.load(fileMuka)
-            faces.extend(user_faces)
+        # Membaca data wajah dan nama
+        file_path = os.path.join(folder_path, file_name)
+        with open(file_path, 'rb') as file:
+            try:
+                data = pickle.load(file)
 
-        # Membaca data nama
-        nama_file_name = 'nama_' + user_name + '.pkl'
-        nama_file_path = os.path.join(folder_path, nama_file_name)
-        with open(nama_file_path, 'rb') as fileNama:
-            user_names = pickle.load(fileNama)
-            names.extend(user_names)
+                # Memeriksa apakah data adalah dictionary
+                if isinstance(data, dict):
+                    user_data = {'faces': data.get('faces', []), 'names': data.get('names', [])}
+                    faces_and_names[user_name] = user_data
+                else:
+                    print(f"Skipping file {file_name} - Invalid data structure.")
+
+            except Exception as e:
+                print(f"Error loading data from file {file_name}: {str(e)}")
 
 # Inisialisasi KNN
 knn = KNeighborsClassifier(n_neighbors=4)
-knn.fit(faces, names)
+
+# Mengumpulkan data wajah dan nama dari semua pengguna
+all_faces = []
+all_names = []
+for user_data in faces_and_names.values():
+    all_faces.extend(user_data['faces'])
+    all_names.extend(user_data['names'])
+
+knn.fit(all_faces, all_names)
 
 # Inisialisasi camera
 camera = cv2.VideoCapture(2)
@@ -45,25 +55,23 @@ camera = cv2.VideoCapture(2)
 csv_filename = 'kehadiran.csv'
 csv_exists = os.path.exists(csv_filename)
 
-with open(csv_filename, 'a', newline='') as csv_file:
-    fieldnames = ['Nama', 'Waktu']
-    writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-
-    if not csv_exists:
-        writer.writeheader()
+fieldnames = ['Nama', 'Waktu']
 
 # Variabel untuk melacak apakah data sudah ditambahkan ke CSV
-data_added = False
-known_names = set(names)  # Mengubah list nama menjadi set untuk pemrosesan yang lebih efisien
+known_names = set(all_names)  # Mengubah list nama menjadi set untuk pemrosesan yang lebih efisien
 current_name = None
 
 while True:
     ret, frame = camera.read()
     if ret == True:
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        face_coordinates = facecascade.detectMultiScale(gray, 1.3, 5)
 
-        for (NxHorizontal, NyVertikal, lebarPembatas, tinggiPembatas) in face_coordinates:
+        # Deteksi wajah frontal
+        face_coordinates_frontal = facecascade_frontal.detectMultiScale(gray, 1.1, 5)
+        # Deteksi wajah profil
+        face_coordinates_profile = facecascade_profile.detectMultiScale(gray, 1.1, 5)
+
+        for (NxHorizontal, NyVertikal, lebarPembatas, tinggiPembatas) in face_coordinates_frontal:
             fc = frame[NyVertikal:NyVertikal + tinggiPembatas, NxHorizontal:NxHorizontal + lebarPembatas, :]
             r = cv2.resize(fc, (50, 50)).flatten().reshape(1, -1)
             text = knn.predict(r)
@@ -71,25 +79,55 @@ while True:
             # Mendapatkan nilai confidence
             confidence = knn.predict_proba(r).max()
 
-            # Menampilkan nama dan confidence pada frame
-            label = f'{text[0]} ({confidence:.2f})'
+            # Menetapkan label "Tidak Diketahui" jika nilai presisi kurang dari 0.8
+            if confidence < 0.8:
+                label = 'Tidak Diketahui'
+            else:
+                label = f'{text[0]} ({confidence:.2f})'
+
+                # Menyimpan data kehadiran ke dalam file CSV hanya jika data belum ditambahkan
+                if text[0] in known_names:
+                    if text[0] != current_name:
+                        current_name = text[0]
+                        data_added = False  # Reset data_added jika nama berbeda
+
+                    if not data_added:
+                        with open(csv_filename, 'a', newline='') as csv_file:
+                            writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+                            writer.writerow({'Nama': text[0], 'Waktu': datetime.now().strftime('%Y-%m-%d %H:%M:%S')})
+                        data_added = True
+
             cv2.putText(frame, label, (NxHorizontal, NyVertikal-10), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 0), 2)
             cv2.rectangle(frame, (NxHorizontal, NyVertikal), (NxHorizontal + lebarPembatas, NyVertikal + tinggiPembatas), (0, 0, 255), 2)
 
-            # cv2.putText(frame, text[0], (NxHorizontal, NyVertikal-10), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 0), 2)
-            # cv2.rectangle(frame, (NxHorizontal, NyVertikal), (NxHorizontal + lebarPembatas, NyVertikal + tinggiPembatas), (0, 0, 255), 2)
+        for (NxHorizontal, NyVertikal, lebarPembatas, tinggiPembatas) in face_coordinates_profile:
+            fc = frame[NyVertikal:NyVertikal + tinggiPembatas, NxHorizontal:NxHorizontal + lebarPembatas, :]
+            r = cv2.resize(fc, (50, 50)).flatten().reshape(1, -1)
+            text = knn.predict(r)
 
-            # Menyimpan data kehadiran ke dalam file CSV hanya jika data belum ditambahkan
-            if text[0] in known_names:
-                if text[0] != current_name:
-                    current_name = text[0]
-                    data_added = False  # Reset data_added jika nama berbeda
+            # Mendapatkan nilai confidence
+            confidence = knn.predict_proba(r).max()
 
-                if not data_added:
-                    with open(csv_filename, 'a', newline='') as csv_file:
-                        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-                        writer.writerow({'Nama': text[0], 'Waktu': datetime.now().strftime('%Y-%m-%d %H:%M:%S')})
-                    data_added = True
+            # Menetapkan label "Tidak Diketahui" jika nilai presisi kurang dari 0.8
+            if confidence < 0.8:
+                label = 'Tidak Diketahui'
+            else:
+                label = f'{text[0]} ({confidence:.2f})'
+
+                # Menyimpan data kehadiran ke dalam file CSV hanya jika data belum ditambahkan
+                if text[0] in known_names:
+                    if text[0] != current_name:
+                        current_name = text[0]
+                        data_added = False  # Reset data_added jika nama berbeda
+
+                    if not data_added:
+                        with open(csv_filename, 'a', newline='') as csv_file:
+                            writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+                            writer.writerow({'Nama': text[0], 'Waktu': datetime.now().strftime('%Y-%m-%d %H:%M:%S')})
+                        data_added = True
+
+            cv2.putText(frame, label, (NxHorizontal, NyVertikal-10), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 0), 2)
+            cv2.rectangle(frame, (NxHorizontal, NyVertikal), (NxHorizontal + lebarPembatas, NyVertikal + tinggiPembatas), (0, 0, 255), 2)
 
         cv2.imshow('Sistem Pengenalan Wajah', frame)
 
@@ -97,7 +135,6 @@ while True:
         key = cv2.waitKey(1) & 0xFF
         if key == ord('q'):
             break
-
     else:
         print("error")
         break
